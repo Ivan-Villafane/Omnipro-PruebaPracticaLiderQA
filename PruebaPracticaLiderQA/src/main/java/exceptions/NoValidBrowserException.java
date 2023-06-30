6b32a509713e32cc5333b86c65b74b79d98ba893
Ivan-Villafane <129786800+Ivan-Villafane@users.noreply.github.com>
2023-06-30 11:35:36 -0500

    Delete PruebaPracticaLiderQA directory

--- PruebaPracticaLiderQA/src/main/java/exceptions/NoValidBrowserException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class NoValidBrowserException extends Exception {
-
-	public NoValidBrowserException(String browser) {
-		super("Invalid browser : '" + browser + "'");
-	}
-
-}
